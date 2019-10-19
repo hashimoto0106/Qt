9284c9f9dc14fb6fbaf3fa8d69dedfa6dd3d35b5
hashimoto naoki <hashimoto0106@gmail.com>
2019-10-19 21:47:31 +0900

    シリアル通信

---
+++ Serial/prj/main.cpp
@@ -0,0 +1,16 @@
+#include "mainwindow.h"
+#include "serialcom.h"
+
+#include <QApplication>
+#include <QDebug>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+    getSerialPortInfo();
+
+    return a.exec();
+}
---
+++ Serial/prj/serialcom.h
@@ -0,0 +1,6 @@
+#ifndef SERIALCOM_H
+#define SERIALCOM_H
+
+void getSerialPortInfo(void);
+
+#endif // SERIALCOM_H
