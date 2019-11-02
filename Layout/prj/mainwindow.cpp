9cfe8b8caa164b0ca5a9976b19d2b30af5e4c6df
hashimoto naoki <hashimoto0106@gmail.com>
2019-11-02 17:52:36 +0900

    add

--- Layout/prj/mainwindow.cpp
+++ Layout/prj/mainwindow.cpp
@@ -1,11 +1,28 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QLabel>
+#include <QPushButton>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QWidget *cw = new QWidget();
+    setCentralWidget(cw);
+
+    //ラベル1
+    QLabel *label_1 = new QLabel(tr("label-1"), cw);
+    label_1->setGeometry(10, 10, 80, 20);
+
+    //ラベル2
+    QLabel *label_2 = new QLabel(tr("label-2"), cw);
+    label_2->setGeometry(10, 40, 80, 20);
+
+    //ボタン1
+    QPushButton *pb1 = new QPushButton(tr("PushButton-1"), cw);
+    pb1->setGeometry(80, 10, 100, 20);
 }
 
 MainWindow::~MainWindow()
