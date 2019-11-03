81aeaf957112a6f2d7005693f746f69f9694d146
hashimoto naoki <hashimoto0106@gmail.com>
2019-11-03 14:47:44 +0900

    QStackedLayout

--- Layout/QStackedLayout/main.cpp
+++ Layout/QStackedLayout/main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    w.testStackedLayout();
     w.show();
     return a.exec();
 }
--- Layout/QStackedLayout/mainwindow.cpp
+++ Layout/QStackedLayout/mainwindow.cpp
@@ -1,5 +1,16 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDialog>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QGridLayout>
+#include <QStackedLayout>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QComboBox>
+#include <QCalendarWidget>
+#include <QPlainTextEdit>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -13,3 +24,28 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::testStackedLayout()
+{
+    QDialog aDlg;
+    QVBoxLayout *vLayout = new QVBoxLayout();
+    QComboBox *pageComboBox = new QComboBox();
+
+    //ページ切り替え
+    pageComboBox->addItem(tr("Page 1"));
+    pageComboBox->addItem(tr("Page 2"));
+    pageComboBox->addItem(tr("Page 3"));
+    vLayout->addWidget(pageComboBox);
+
+    QStackedLayout *stackedLayout = new QStackedLayout();
+    stackedLayout->addWidget(new QCalendarWidget);
+    stackedLayout->addWidget(new QDialog);
+    stackedLayout->addWidget(new QPlainTextEdit);
+
+    vLayout->addLayout(stackedLayout);
+
+    connect(pageComboBox, SIGNAL(activated(int)), stackedLayout, SLOT(setCurrentIndex(int)));
+
+    aDlg.setLayout(vLayout);
+    aDlg.exec();
+}
