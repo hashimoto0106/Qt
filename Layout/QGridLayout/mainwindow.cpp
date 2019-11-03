b3d68a65d28ad921d7ba14e3f0c7976c5c1f0ea5
hashimoto naoki <hashimoto0106@gmail.com>
2019-11-03 11:38:09 +0900

    QGridLayout

--- Layout/QGridLayout/mainwindow.cpp
+++ Layout/QGridLayout/mainwindow.cpp
@@ -1,5 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDialog>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QGridLayout>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -13,3 +20,21 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::testGridLayout()
+{
+    QDialog aDlg;
+    QVBoxLayout *vLayout = new QVBoxLayout();
+    QGridLayout *gLayout = new QGridLayout();
+
+    gLayout->addWidget(new QLabel(tr("hoge")), 0, 0, Qt::AlignRight);
+    gLayout->addWidget(new QLineEdit(), 0, 1, Qt::AlignRight);
+    gLayout->addWidget(new QLabel(tr("hogeHoge")), 1, 0, Qt::AlignRight);
+    gLayout->addWidget(new QLineEdit(), 1, 1, Qt::AlignRight);
+
+    vLayout->addLayout(gLayout);
+    vLayout->addStretch();
+
+    aDlg.setLayout(vLayout);
+    aDlg.exec();
+}
