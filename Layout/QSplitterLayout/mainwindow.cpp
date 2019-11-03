1375723c70d5943659bf4b494154bd2bc93f1eae
hashimoto naoki <hashimoto0106@gmail.com>
2019-11-03 17:53:34 +0900

    QSplitterLayout

--- Layout/QSplitterLayout/mainwindow.cpp
+++ Layout/QSplitterLayout/mainwindow.cpp
@@ -1,5 +1,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDialog>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QSplitter>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QTreeWidget>
+#include <QListWidget>
+#include <QPlainTextEdit>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -13,3 +23,17 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::testSplitter()
+{
+    QDialog aDlg;
+    QSplitter *hSplitter = new QSplitter(Qt::Horizontal);
+    hSplitter->addWidget(m_tree = new QTreeWidget);
+    QSplitter *vSplitter = new QSplitter(Qt::Vertical);
+    hSplitter->addWidget(vSplitter);
+    vSplitter->addWidget(m_list = new QListWidget);
+    vSplitter->addWidget(m_editor = new QPlainTextEdit);
+    setCentralWidget(hSplitter)
+    aDlg.setLayout(vSplitter);
+    aDlg.exec();
+}
--- Layout/QSplitterLayout/mainwindow.h
+++ Layout/QSplitterLayout/mainwindow.h
@@ -14,6 +14,7 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    void testSplitter();
 
 private:
     Ui::MainWindow *ui;
