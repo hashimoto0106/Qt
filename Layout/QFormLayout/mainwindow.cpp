01e60362d06d2845cca54cc4b623a5b5232184a5
hashimoto naoki <hashimoto0106@gmail.com>
2019-11-03 11:57:45 +0900

    QFormLayout

--- Layout/QFormLayout/mainwindow.cpp
+++ Layout/QFormLayout/mainwindow.cpp
@@ -1,5 +1,13 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDialog>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QGridLayout>
+#include <QFormLayout>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -13,3 +21,21 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::testFormLayout()
+{
+    QDialog aDlg;
+    QVBoxLayout *vLayout = new QVBoxLayout();
+    QFormLayout *fLayout = new QFormLayout();
+
+    fLayout->setLabelAlignment(Qt::AlignRight);
+
+    fLayout->addRow(tr("hoge"), new QLineEdit());
+    fLayout->addRow(tr("hogeHoge"), new QLineEdit());
+
+    vLayout->addLayout(fLayout);
+    vLayout->addStretch();
+
+    aDlg.setLayout(vLayout);
+    aDlg.exec();
+}
