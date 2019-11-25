47f1b41d7734a19afc6d5b2004a2571bdff302c5
hashimoto naoki <hashimoto0106@gmail.com>
2019-11-25 22:35:47 +0900

    tablewidget(監視画面)

--- Table/build-prj-Desktop_Qt_5_12_2_MinGW_64_bit-Debug/ui_mainwindow.h
+++ Table/build-prj-Desktop_Qt_5_12_2_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -11,9 +11,14 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLineEdit>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTableView>
+#include <QtWidgets/QTableWidget>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -22,6 +27,12 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralwidget;
+    QTableView *tableView;
+    QTableWidget *tableWidget;
+    QLineEdit *lineEdit;
+    QPushButton *pushButton;
+    QPushButton *pushButton_2;
+    QPushButton *pushButton_3;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -32,9 +43,64 @@ public:
         MainWindow->resize(800, 600);
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        tableView = new QTableView(centralwidget);
+        tableView->setObjectName(QString::fromUtf8("tableView"));
+        tableView->setGeometry(QRect(490, 60, 101, 192));
+        tableWidget = new QTableWidget(centralwidget);
+        if (tableWidget->columnCount() < 5)
+            tableWidget->setColumnCount(5);
+        if (tableWidget->rowCount() < 3)
+            tableWidget->setRowCount(3);
+        QTableWidgetItem *__qtablewidgetitem = new QTableWidgetItem();
+        tableWidget->setItem(0, 0, __qtablewidgetitem);
+        QTableWidgetItem *__qtablewidgetitem1 = new QTableWidgetItem();
+        tableWidget->setItem(0, 1, __qtablewidgetitem1);
+        QTableWidgetItem *__qtablewidgetitem2 = new QTableWidgetItem();
+        tableWidget->setItem(0, 2, __qtablewidgetitem2);
+        QTableWidgetItem *__qtablewidgetitem3 = new QTableWidgetItem();
+        tableWidget->setItem(0, 3, __qtablewidgetitem3);
+        QTableWidgetItem *__qtablewidgetitem4 = new QTableWidgetItem();
+        tableWidget->setItem(0, 4, __qtablewidgetitem4);
+        QTableWidgetItem *__qtablewidgetitem5 = new QTableWidgetItem();
+        tableWidget->setItem(1, 0, __qtablewidgetitem5);
+        QTableWidgetItem *__qtablewidgetitem6 = new QTableWidgetItem();
+        tableWidget->setItem(1, 1, __qtablewidgetitem6);
+        QTableWidgetItem *__qtablewidgetitem7 = new QTableWidgetItem();
+        tableWidget->setItem(1, 2, __qtablewidgetitem7);
+        QTableWidgetItem *__qtablewidgetitem8 = new QTableWidgetItem();
+        tableWidget->setItem(1, 3, __qtablewidgetitem8);
+        QTableWidgetItem *__qtablewidgetitem9 = new QTableWidgetItem();
+        tableWidget->setItem(1, 4, __qtablewidgetitem9);
+        QTableWidgetItem *__qtablewidgetitem10 = new QTableWidgetItem();
+        tableWidget->setItem(2, 0, __qtablewidgetitem10);
+        QTableWidgetItem *__qtablewidgetitem11 = new QTableWidgetItem();
+        tableWidget->setItem(2, 1, __qtablewidgetitem11);
+        QTableWidgetItem *__qtablewidgetitem12 = new QTableWidgetItem();
+        tableWidget->setItem(2, 2, __qtablewidgetitem12);
+        QTableWidgetItem *__qtablewidgetitem13 = new QTableWidgetItem();
+        tableWidget->setItem(2, 3, __qtablewidgetitem13);
+        QTableWidgetItem *__qtablewidgetitem14 = new QTableWidgetItem();
+        tableWidget->setItem(2, 4, __qtablewidgetitem14);
+        tableWidget->setObjectName(QString::fromUtf8("tableWidget"));
+        tableWidget->setGeometry(QRect(30, 30, 451, 371));
+        tableWidget->setRowCount(3);
+        tableWidget->setColumnCount(5);
+        lineEdit = new QLineEdit(centralwidget);
+        lineEdit->setObjectName(QString::fromUtf8("lineEdit"));
+        lineEdit->setGeometry(QRect(30, 470, 113, 20));
+        pushButton = new QPushButton(centralwidget);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+        pushButton->setGeometry(QRect(190, 470, 75, 23));
+        pushButton_2 = new QPushButton(centralwidget);
+        pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
+        pushButton_2->setGeometry(QRect(290, 470, 75, 23));
+        pushButton_3 = new QPushButton(centralwidget);
+        pushButton_3->setObjectName(QString::fromUtf8("pushButton_3"));
+        pushButton_3->setGeometry(QRect(390, 470, 75, 23));
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 800, 24));
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
@@ -48,6 +114,44 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", nullptr));
+
+        const bool __sortingEnabled = tableWidget->isSortingEnabled();
+        tableWidget->setSortingEnabled(false);
+        QTableWidgetItem *___qtablewidgetitem = tableWidget->item(0, 0);
+        ___qtablewidgetitem->setText(QApplication::translate("MainWindow", "1,1", nullptr));
+        QTableWidgetItem *___qtablewidgetitem1 = tableWidget->item(0, 1);
+        ___qtablewidgetitem1->setText(QApplication::translate("MainWindow", "1,2", nullptr));
+        QTableWidgetItem *___qtablewidgetitem2 = tableWidget->item(0, 2);
+        ___qtablewidgetitem2->setText(QApplication::translate("MainWindow", "1,3", nullptr));
+        QTableWidgetItem *___qtablewidgetitem3 = tableWidget->item(0, 3);
+        ___qtablewidgetitem3->setText(QApplication::translate("MainWindow", "1,4", nullptr));
+        QTableWidgetItem *___qtablewidgetitem4 = tableWidget->item(0, 4);
+        ___qtablewidgetitem4->setText(QApplication::translate("MainWindow", "1,5", nullptr));
+        QTableWidgetItem *___qtablewidgetitem5 = tableWidget->item(1, 0);
+        ___qtablewidgetitem5->setText(QApplication::translate("MainWindow", "2,1", nullptr));
+        QTableWidgetItem *___qtablewidgetitem6 = tableWidget->item(1, 1);
+        ___qtablewidgetitem6->setText(QApplication::translate("MainWindow", "2,2", nullptr));
+        QTableWidgetItem *___qtablewidgetitem7 = tableWidget->item(1, 2);
+        ___qtablewidgetitem7->setText(QApplication::translate("MainWindow", "2,3", nullptr));
+        QTableWidgetItem *___qtablewidgetitem8 = tableWidget->item(1, 3);
+        ___qtablewidgetitem8->setText(QApplication::translate("MainWindow", "2,4", nullptr));
+        QTableWidgetItem *___qtablewidgetitem9 = tableWidget->item(1, 4);
+        ___qtablewidgetitem9->setText(QApplication::translate("MainWindow", "2,5", nullptr));
+        QTableWidgetItem *___qtablewidgetitem10 = tableWidget->item(2, 0);
+        ___qtablewidgetitem10->setText(QApplication::translate("MainWindow", "3,1", nullptr));
+        QTableWidgetItem *___qtablewidgetitem11 = tableWidget->item(2, 1);
+        ___qtablewidgetitem11->setText(QApplication::translate("MainWindow", "3,2", nullptr));
+        QTableWidgetItem *___qtablewidgetitem12 = tableWidget->item(2, 2);
+        ___qtablewidgetitem12->setText(QApplication::translate("MainWindow", "3,3", nullptr));
+        QTableWidgetItem *___qtablewidgetitem13 = tableWidget->item(2, 3);
+        ___qtablewidgetitem13->setText(QApplication::translate("MainWindow", "3,4", nullptr));
+        QTableWidgetItem *___qtablewidgetitem14 = tableWidget->item(2, 4);
+        ___qtablewidgetitem14->setText(QApplication::translate("MainWindow", "3,5", nullptr));
+        tableWidget->setSortingEnabled(__sortingEnabled);
+
+        pushButton->setText(QApplication::translate("MainWindow", "PushButton", nullptr));
+        pushButton_2->setText(QApplication::translate("MainWindow", "PushButton", nullptr));
+        pushButton_3->setText(QApplication::translate("MainWindow", "PushButton", nullptr));
     } // retranslateUi
 
 };
--- Table/prj/mainwindow.cpp
+++ Table/prj/mainwindow.cpp
@@ -1,11 +1,79 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QTableWidget.h>
+
+#define D_MAX_COL 4
+#define D_MAX_ROW 7
+
+
+#define COL_1 0
+#define COL_2 1
+#define COL_3 2
+#define COL_4 3
+#define COL_5 4
+#define COL_6 5
+#define COL_MAX 6
+
+int m_row = 0;
+int count = 0;
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    // ラベル１のlineEditに文字を表示する。
+    ui->lineEdit->setText("TEST");
+
+    //——————————
+    //ボタンイベント登録
+    //——————————
+//    QObject::connect(ui->pushButton, SIGNAL(clicked()), this, SLOT(clickPshBtn()));
+
+    //——————————
+    //テーブル生成
+    //——————————
+    QTableWidget* tblWdgt;
+    tblWdgt = ui->tableWidget;
+
+    // クリア
+    tblWdgt->clear();
+
+    // テーブルの列数、行数を設定する
+    tblWdgt->setColumnCount( D_MAX_COL );
+    tblWdgt->setRowCount( D_MAX_ROW );
+
+    // 列のタイトル設定
+    tblWdgt->setHorizontalHeaderLabels(
+     QStringList()
+      << tr("Title 1")
+      << tr("Title 2" )
+      << tr("Title 3" )
+      << tr("Title 4" )
+      << tr("Title 5") );
+
+    //——————————
+    //テーブルプロパティ設定
+    //——————————
+    //セル選択不可にする場合
+    tblWdgt->setSelectionMode(QAbstractItemView::NoSelection);
+
+    //いわゆるExcelみたいなタイプ。Ctrl＋クリックで飛び地の選択も可能。
+    tblWdgt->setSelectionMode(QAbstractItemView::ExtendedSelection);
+
+    //ダブルクリックでの編集不可にする
+    tblWdgt->setEditTriggers(QAbstractItemView::NoEditTriggers);
+
+    //カラム[0]の列幅設定
+    tblWdgt->setColumnWidth(COL_1,100);
+
+    // 選択したセルの色をデフォルトのグレーから変更する
+    // 親のカラーパレットを取得し変更する
+    //Qt::red, Qt::blue, Qt::green, Qt::yellow, Qt::magenta, Qt::cyan
+    QPalette palette = tblWdgt->palette();
+    palette.setColor(QPalette::Highlight, Qt::red);//現在選択されている項目の背景色
+    palette.setColor(QPalette::HighlightedText,Qt::green);//現在選択されている項目の文字色
+    tblWdgt->setPalette(palette);
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +81,33 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_pushButton_clicked()
+{
+    QTableWidget* tblWdgt = ui->tableWidget;
+    //最大行数の場合、先頭行の削除
+    if(m_row >= D_MAX_ROW){
+        tblWdgt->removeRow(0);
+        m_row--;
+
+        //最終行追加
+        tblWdgt->insertRow(m_row);
+    }
+
+    //データを追加する
+    count++;
+    QTableWidgetItem *item[COL_MAX];
+    item[COL_1] = new QTableWidgetItem(QString("data1 #%8.0f").arg(0.1));
+    item[COL_2] = new QTableWidgetItem(QString("data2 #%8.0f").arg(0.1));
+    item[COL_3] = new QTableWidgetItem(QString("data3 #%8.0f").arg(0.1));
+    item[COL_4] = new QTableWidgetItem(QString("data4 #%8.0f").arg(0.1));
+    item[COL_5] = new QTableWidgetItem(QString("data5 #%8.0f").arg(0.1));
+    for(int ncol=0;ncol<COL_MAX;ncol++){ tblWdgt->setItem(m_row, ncol, item[ncol]);}
+
+    // 指定したアイテム（行）までスクロール(列は未指定）
+    QTableWidgetItem *pItem = tblWdgt->item( m_row, 0 );
+    tblWdgt->scrollToItem( pItem );
+
+    //行インクリメント
+    m_row++;
+}
