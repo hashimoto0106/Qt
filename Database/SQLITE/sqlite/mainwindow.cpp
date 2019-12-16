e895c3c2d237bcbd0b2fe2703948355b031a9569
hashimoto naoki <hashimoto0106@gmail.com>
2019-12-16 23:14:05 +0900

    Database

--- Database/SQLITE/sqlite/mainwindow.cpp
+++ Database/SQLITE/sqlite/mainwindow.cpp
@@ -1,11 +1,39 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    db = QSqlDatabase::addDatabase("QSQLITE" , "CONNECTION NAME");
+    db.setDatabaseName("C:\\sqlite_db_file.sqlite");
+    if(!db.open())
+    {
+        qDebug() << "Can't Connect to DB !";
+    }
+    else
+    {
+        qDebug() << "Connected Successfully to DB !";
+        QSqlQuery query;
+        query.prepare("SELECT name , phone , address FROM employees WHERE ID = 201");
+        if(!query.exec())
+        {
+            qDebug() << "Can't Execute Query !";
+        }
+        else
+        {
+            qDebug() << "Query Executed Successfully !";
+            while(query.next())
+            {
+                qDebug() << "Employee Name : " << query.value(0).toString();
+                qDebug() << "Employee Phone Number : " << query.value(1).toString();
+                qDebug() << "Employee Address : " << query.value(1).toString();
+            }
+        }
+    }
 }
 
 MainWindow::~MainWindow()
--- Database/SQLITE/sqlite/mainwindow.h
+++ Database/SQLITE/sqlite/mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QSql>
+//#include <QSqlDatabase>
+#include <QDebug>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,5 +20,6 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QSqlDatabase db;
 };
 #endif // MAINWINDOW_H
